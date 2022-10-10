0606388e916e7374952b64d4922f907ad4ac57dd
ho4607 <psh30095@gmail.com>
2022-10-10 22:28:58 +0900

    :zap: 물내림 정보 확인 api 추가

--- src/routes/api/places-status/controller.js
+++ src/routes/api/places-status/controller.js
@@ -6,11 +6,34 @@ import {
     UPDATE_CLEAN_PLACE_STATUS_LOG
 } from "@/database/queries/place-status";
 
+export const findUpdatedCleanHistory = wrapperAsync(async (req,res)=>{
+    const {place_id:placeId, model_id:modelId} = req.params;
+    const {person_clean:personClean} = req.query
+
+    if(personClean.toUpperCase()==='TRUE'){
+        let {id, date_clean:dateClean, time_clean:timeClean} = await sendRawQuery(FIND_PLACE_STATUS_LOG, {
+            placeId, modelId
+        })
+
+        const result = {
+            success:true,
+            data: {
+                id,
+                date: dateClean,
+                time: timeClean
+            }
+        }
+        res.status(200).json(result)
+
+    }
+    else throw new Error('Bad Request.');
+})
+
 export const updateCleanHistory = wrapperAsync( async(req,res)=>{
     const {place_id:placeId, model_id:modelId} = req.params;
     const {person_clean:personClean} = req.body
 
-    if(personClean.toUpperCase()==='TRUE'){
+    if(personClean?.toUpperCase()==='TRUE'){
         await sendRawQuery( UPDATE_CLEAN_PLACE_STATUS_LOG,
             {
                 placeId, modelId,
@@ -39,7 +62,7 @@ export const findNewPlaceStatus = wrapperAsync( async(req,res)=>{
     const {place_id:placeId, model_id:modelId}=req.params;
     const {person_in:personIn} = req.query;
 
-    if(personIn.toUpperCase() === 'TRUE') {
+    if(personIn?.toUpperCase() === 'TRUE') {
         let {id, date_in:dateIn, time_in:timeIn} = await sendRawQuery(FIND_PLACE_STATUS_LOG, {
             placeId, modelId
         })
@@ -52,7 +75,7 @@ export const findNewPlaceStatus = wrapperAsync( async(req,res)=>{
                 time: timeIn
             }
         }
-        res.status(201).json(result)
+        res.status(200).json(result)
     }
     else throw new Error('Bad Request.');
 })
--- src/routes/api/places-status/index.js
+++ src/routes/api/places-status/index.js
@@ -1,11 +1,16 @@
 import express from 'express'
-import {createNewPlaceStatus, findNewPlaceStatus, updateCleanHistory} from "@/routes/api/places-status/controller";
+import {
+    createNewPlaceStatus,
+    findNewPlaceStatus,
+    findUpdatedCleanHistory,
+    updateCleanHistory
+} from "@/routes/api/places-status/controller";
 
 const place = express.Router()
 
 place.post('/in/:place_id(\\d+)/:model_id(\\d+)',createNewPlaceStatus)
 place.get('/in/:place_id(\\d+)/:model_id(\\d+)',findNewPlaceStatus)
-// place.post('/out/:id(\\d+)',updatePlaceState)
 place.post('/clean/:place_id(\\d+)/:model_id(\\d+)',updateCleanHistory)
+place.get('/clean/:place_id(\\d+)/:model_id(\\d+)',findUpdatedCleanHistory)
 
 module.exports = place;
